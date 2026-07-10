420032ddeabccdec4e76cd3061ba13c8a5613b17
rhyde-a11y <rhyde@perscholas.org>
2026-07-10 18:44:45 -0400

    updates to error text

--- sentinel2.py
+++ sentinel2.py
@@ -7,8 +7,8 @@ while guess != Correct_password and attempts < max_attempts:
     guess=input("Enter password: ")
     if guess != Correct_password:
         attempts += 1
-        remaining=max_attempts - attempts
-        print(f"Incorrect! Attempts remaining: {remaining}")
+        remaining=max_attempts-attempts
+        print(f"Try again! Attempts remaining: {remaining}")
 
 if guess == Correct_password:
     print("Access permitted.")
