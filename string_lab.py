f9bfc879b00847b16c3594d12f633ae7c767f0b1
rhyde-a11y <rhyde@perscholas.org>
2026-07-22 19:39:28 -0400

    daily labs update

--- string_lab.py
+++ string_lab.py
@@ -4,6 +4,7 @@ print(len(name))
 f_n = 'Kelly'
 l_n = 'Hyde'
 print(f_n + ' ' + l_n)
+
 print(('Kelly 🤯 '*4) + ('Hyde 😎 ' * 3))
 
 name = 'Kelly Hyde'
@@ -14,6 +15,9 @@ print()
 
 name = 'Kelly Hyde'
 for ix in range(len(name)):
-    print(name[ix], end='_')
+    print(name[ix].upper(), end='_')
     
 print()
+
+test = "476"
+print(test)
